a7840649b65e0dd3d4e7ee33cb3c5c88eab89507
sunao1222 <g024c1054@g.neec.ac.jp>
2026-05-14 09:34:23 +0900

    add:ssh-key追加

---
+++ ssh_key.tf
@@ -0,0 +1,8 @@
+resource "harvester_ssh_key" "sunao1222-key" {
+  description = null
+  labels      = {}
+  name        = "sunao1222-key"
+  namespace   = "default"
+  public_key  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJs2H6rAqEu086/vvwhuUXZXFoJb2ovK0FTiT1ieax72 Iras@ThinkBook-E14-G7"
+  tags        = {}
+}
